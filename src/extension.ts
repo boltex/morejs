569140cb709b11f7b0996dc0a023a77f75499a96
Félix <felixgraphx@gmail.com>
2021-03-14 00:53:23 -0500

    cleaned up before making body switching experiments

--- src/extension.ts
+++ src/extension.ts
@@ -10,60 +10,58 @@ import { MoreOutlineProvider, MoreNode } from './moreOutline';
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Starting morejs activation');
 
-	// TREEVIEW SAMPLE EXAMPLE
+	// SHORTCUTS *******************************************************************************
+	function subPush(p_disposable: vscode.Disposable) {
+		context.subscriptions.push(p_disposable);
+	}
+	const regTree = vscode.window.registerTreeDataProvider;
+	const regCmd = vscode.commands.registerCommand;
+	const regFileSys = vscode.workspace.registerFileSystemProvider;
+
+	// TREEVIEW SAMPLE EXAMPLE *****************************************************************
 	const jsonOutlineProvider = new JsonOutlineProvider(context);
+	subPush(regTree('jsonOutline', jsonOutlineProvider));
+	subPush(regCmd('morejs.refresh', () => jsonOutlineProvider.refresh()));
+	subPush(regCmd('morejs.refreshNode', offset => jsonOutlineProvider.refresh(offset)));
+	subPush(regCmd('morejs.renameNode', offset => jsonOutlineProvider.rename(offset)));
 
-	context.subscriptions.push(vscode.window.registerTreeDataProvider('jsonOutline', jsonOutlineProvider));
-	context.subscriptions.push(vscode.commands.registerCommand('morejs.refresh', () => jsonOutlineProvider.refresh()));
-	context.subscriptions.push(vscode.commands.registerCommand('morejs.refreshNode', offset => jsonOutlineProvider.refresh(offset)));
-	context.subscriptions.push(vscode.commands.registerCommand('morejs.renameNode', offset => jsonOutlineProvider.rename(offset)));
-
-	// MOREJS Body pane implementation
+	// MOREJS BODY PANE IMPLEMENTATION *********************************************************
 	const scheme: string = "more";
 	let bodyTextDocument: vscode.TextDocument | undefined;
 	let _bodyLastChangedDocument: vscode.TextDocument | undefined; // Only set in _onDocumentChanged
-
 	let bodyUri: vscode.Uri = strToMoreUri("");
-
 	const moreOutlineProvider = new MoreOutlineProvider(context);
-	context.subscriptions.push(vscode.window.registerTreeDataProvider('jsOutline', moreOutlineProvider));
-	vscode.commands.executeCommand('setContext', 'jsOutlineEnabled', true);
-
 	const _leoFileSystem = new JsBodyProvider(moreOutlineProvider);
 
-	context.subscriptions.push(
-		vscode.workspace.registerFileSystemProvider(scheme, _leoFileSystem, { isCaseSensitive: true })
-	);
+	subPush(regTree('jsOutline', moreOutlineProvider));
+	subPush(regFileSys(scheme, _leoFileSystem, { isCaseSensitive: true }));
+	subPush(regCmd('morejs.selectNode', p_JsNode => selectNode(p_JsNode)));
 
-	vscode.commands.registerCommand('morejs.selectNode', (p_JsNode: MoreNode) => {
-		console.log('SHOW BODY GNX: ', p_JsNode.pnode.gnx);
+	vscode.window.onDidChangeActiveTextEditor(p_event => triggerBodySave()); // also fires when the active editor becomes undefined
+	vscode.window.onDidChangeTextEditorViewColumn(() => triggerBodySave()); // also triggers after drag and drop
+	vscode.window.onDidChangeVisibleTextEditors(() => triggerBodySave()); // window.visibleTextEditors changed
+	vscode.window.onDidChangeWindowState(() => triggerBodySave()); // focus state of the current window changes
+	vscode.workspace.onDidChangeTextDocument(p_event => _onDocumentChanged(p_event)); // typing and changing body
+
+	function selectNode(p_JsNode: MoreNode): void {
+		console.log('SELECT NODE GNX: ', p_JsNode.pnode.gnx);
 		bodyUri = strToMoreUri(p_JsNode.pnode.gnx);// via global
 		showBody();
 		moreOutlineProvider.lastSelectedNode = p_JsNode;
-	});
+	}
 
 	function showBody(): Promise<vscode.TextEditor> {
 		return Promise.resolve(vscode.workspace.openTextDocument(bodyUri)).then(p_document => {
 			bodyTextDocument = p_document;
-
-			const w_showOptions: vscode.TextDocumentShowOptions = {
-				viewColumn: 1, // view column in which the editor should be shown
-				preserveFocus: false, // an optional flag that when true will stop the editor from taking focus
-				preview: true // should text document be in preview only? set false for fully opened
-				// selection is instead set when the GET_BODY_STATES above resolves
-			};
-
+			const w_showOptions: vscode.TextDocumentShowOptions = { viewColumn: 1, preserveFocus: false, preview: true };
 			return vscode.window.showTextDocument(bodyTextDocument, w_showOptions).then(w_bodyEditor => {
-				// w_bodyEditor.options.lineNumbers = OFFSET ;
 				return Promise.resolve(w_bodyEditor);
 			});
 		});
-
 	}
 
 	function _bodySaveDocument(p_document: vscode.TextDocument, p_forcedVsCodeSave?: boolean): Promise<boolean> {
 		console.log('SAVE BODY BACK TO NODE!');
-
 		return Promise.resolve(true);
 	}
 
@@ -89,25 +87,7 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	}
 
-	function _onActiveEditorChanged(p_event: vscode.TextEditor | undefined) {
-
-		triggerBodySave();
-	}
-
-	// * React to change in active panel/text editor (window.activeTextEditor) - also fires when the active editor becomes undefined
-	vscode.window.onDidChangeActiveTextEditor(p_event => _onActiveEditorChanged(p_event));
-
-	// * Triggers when a different text editor/vscode window changed focus or visibility, or dragged
-	// This is also what triggers after drag and drop, see '_onChangeEditorViewColumn'
-	vscode.window.onDidChangeTextEditorViewColumn(() => triggerBodySave());
-	vscode.window.onDidChangeVisibleTextEditors(() => triggerBodySave());
-	vscode.window.onDidChangeWindowState(() => triggerBodySave());
-
-	// * React when typing and changing body pane
-	vscode.workspace.onDidChangeTextDocument(p_event => _onDocumentChanged(p_event));
-
-	// * React to opening of any file in vscode
-	// vscode.workspace.onDidOpenTextDocument(p_document => _onDidOpenTextDocument(p_document));
+	vscode.commands.executeCommand('setContext', 'moreOutlineEnabled', true);
 
 	console.log('Finished activating morejs');
 }
